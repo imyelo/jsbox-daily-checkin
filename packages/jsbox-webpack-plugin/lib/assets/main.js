7d618d004ff62b5903b3d15ed20772482ca1d3d2
yelo <zhihuzeye@gmail.com>
2019-11-13 01:07:25 +0800

    refactor: move jsbox-webpack-plugin to independent repo

--- packages/jsbox-webpack-plugin/lib/assets/main.js
+++
@@ -1,9 +0,0 @@
-/**
-  * DO NOT MODIFY THIS FILE DIRECTLY.
-  * JUST WRITE CODES IN `SCRIPTS` DIRECTORY AND EXPORT FROM `SCRIPTS/MAIN.JS`.
-  * GLHF.
-  * ---
-  * Created with JSBoxPlugin
-  */
-
- require('./scripts/main')
