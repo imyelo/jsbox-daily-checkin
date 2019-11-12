0635a8878473717068766d76cbc14e84868236d5
yelo <zhihuzeye@gmail.com>
2019-11-12 21:26:33 +0800

    refactor: jsbox-webpack-plugin
    - builtin zip func via yazl without gulp
    - builtin copy func instead of extra copy-webpack-plugin
    - builtin `main.js` generator

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const { resolve } = require('path')
-const CopyPlugin = require('copy-webpack-plugin')
 const JSBoxPlugin = require('jsbox-webpack-plugin')
 
 const isProduction = process.env.NODE_ENV === 'production'
@@ -9,23 +8,15 @@ module.exports = {
   entry: './scripts/main.js',
   output: {
     path: resolve(`dist/${require('./src/config.json').info.name}`),
-    filename: './scripts/main.js',
+    filename: 'scripts/main.js',
     globalObject: 'global',
   },
   resolve: {
     symlinks: true,
   },
   plugins: [
-    new CopyPlugin([
-      { from: resolve('readme.md'), to: 'README.md' },
-      { from: resolve('readme-en.md'), to: 'README_EN.md' },
-      { from: 'main.js', to: 'main.js' },
-      { from: 'config.json', to: 'config.json' },
-      { from: 'strings', to: 'strings' },
-      { from: 'assets', to: 'assets' },
-    ]),
     new JSBoxPlugin({
-      sync: !isProduction,
+      upload: !isProduction,
     }),
   ],
   mode: isProduction ? 'production' : 'none',
